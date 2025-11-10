098bd2776e75d57f7578366ec57b9c2ec44f0055
zaid <zaidalmoughrabi@gmail.com>
2025-11-10 10:39:57 +0300

    Remove unused components and assets

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
-import Header from "./components/Header";
-import Post from "./components/Post";
-import SideMenu from "./components/SideMenu";
-
 function App() {
-  const posts = [
-    {
-      key: 1,
-      Title: `Test 1`,
-      Body: `Body 1`,
-    },
-    {
-      key: 2,
-      Title: `Test 2`,
-      Body: `Body 2`,
-    },
-    {
-      key: 3,
-      Title: `Test 3`,
-      Body: `Body 3`,
-    },
-    {
-      key: 4,
-      Title: `Test 4`,
-      Body: `Body 4`,
-    },
-  ];
-  const postsList = posts.map((post) => (
-    <Post key={post.key} Title={post.Title} Body={post.Body} />
-  ));
-  return (
-    <div>
-      <Header />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ width: "60%", display: "flex" }}>
-          <div style={{ width: "70%" }}>{postsList} </div>
-          <div style={{ width: "30%" }}>
-            <SideMenu />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/TagButton.js
+++
@@ -1,5 +0,0 @@
-import "../Styles/TagButtonStyle.css";
-
-export default function TagButton({ children }) {
-  return <button className="tagButton">{children}</button>;
-}
